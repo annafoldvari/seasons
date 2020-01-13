1d837ea5a8b45fdb39167a2813d867697f1f5e5d
anna_f <annafoldvari@gmx.co.uk>
2020-01-13 12:51:04 +0000

    Finishing App

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 class App extends React.Component {
   // constructor(props) {
@@ -22,7 +23,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -31,7 +32,15 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat}/>
     }
 
-    return <div>Loading!</div>
+    return <Spinner message="Pleasde accept the location request"/>
+  }
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    )
 
   }
 }
